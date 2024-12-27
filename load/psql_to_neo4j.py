cfc3e1d180efb5fa6606dcaa551647ebb7d7c474
ophiase <ophiase@outlook.com>
2024-12-27 17:06:37 +0100

    implemented: neo4j: Work, Akas, Episode, Genre, WorkType

--- load/psql_to_neo4j.py
+++ load/psql_to_neo4j.py
@@ -6,7 +6,6 @@ from dataclasses import dataclass
 
 ###################################################################################
 
-
 @dataclass
 class Work:
     id: int
@@ -18,8 +17,35 @@ class Work:
     end_year: int
     runtime_minutes: int
 
-###################################################################################
+@dataclass
+class Akas:
+    id_work: int
+    ordering: int
+    title: str
+    region: str
+    language: str
+    attributes: str
+    is_original_title: int
 
+@dataclass
+class Episode:
+    id_work: int
+    id_work_parent: int
+    season_number: int
+    episode_number: int
+
+@dataclass
+class Genre:
+    id_work: int
+    genre: str
+
+@dataclass
+class WorkType:
+    id_work: int
+    ordering: int
+    type: str
+
+###################################################################################
 
 def connect_postgresql():
     return connect(
@@ -27,7 +53,6 @@ def connect_postgresql():
         user=PG_USER,
         password=PG_PASSWORD,
     )
-
 
 def connect_neo4j() -> Graph:
     graph = Graph(NEO4J_HOST, auth=NEO4J_AUTH)
@@ -71,50 +96,134 @@ def migrate_work(pg_connect, graph) -> None:
         print("Inserted works into Neo4j")
 
 
-def migrate_akas(pg_connect, graph) -> None: pass
-"""
+def migrate_akas(pg_connect, graph) -> None:
+    with pg_connect.cursor(name='akas_cursor') as cur:
+        cur.execute(
+            """
             SELECT 
             id_work, ordering, title, region, language, attributes, is_original_title
             FROM work_akas
-            """
-# for this one, you will create a NodeType Akas
-# then create the relation (:Work)-[:HAS_AKAS]->(:Akas)
+            """)
 
-def migrate_episode(pg_connect, graph) -> None: pass
-"""
+        while True:
+            akas_list = cur.fetchmany(1000)
+            if not akas_list:
+                break
+
+            for akas_data in akas_list:
+                akas = Akas(*akas_data)
+                akas_node = Node("Akas",
+                                 id_work=akas.id_work,
+                                 ordering=akas.ordering,
+                                 title=akas.title,
+                                 region=akas.region,
+                                 language=akas.language,
+                                 attributes=akas.attributes,
+                                 is_original_title=akas.is_original_title)
+                graph.create(akas_node)
+                work_node = graph.nodes.match("Work", id=akas.id_work).first()
+                if work_node:
+                    graph.create(Relationship(work_node, "HAS_AKAS", akas_node))
+        print("Inserted akas into Neo4j")
+
+def migrate_episode(pg_connect, graph) -> None:
+    with pg_connect.cursor(name='episode_cursor') as cur:
+        cur.execute(
+            """
             SELECT 
             id_work, id_work_parent, season_number, episode_number
             FROM work_episode
+            """)
+
+        episodes = []
+        while True:
+            episode_batch = cur.fetchmany(1000)
+            if not episode_batch:
+                break
+            episodes.extend(episode_batch)
+
+        episode_nodes = {}
+        for episode_data in episodes:
+            episode = Episode(*episode_data)
+            episode_node = Node("Episode",
+                                id_work=episode.id_work,
+                                id_work_parent=episode.id_work_parent,
+                                season_number=episode.season_number,
+                                episode_number=episode.episode_number)
+            graph.create(episode_node)
+            episode_nodes[(episode.id_work_parent, episode.season_number, episode.episode_number)] = episode_node
+
+            work_node = graph.nodes.match("Work", id=episode.id_work).first()
+            parent_work_node = graph.nodes.match("Work", id=episode.id_work_parent).first()
+
+            if work_node:
+                graph.create(Relationship(episode_node, "IS_WORK", work_node))
+            if parent_work_node:
+                graph.create(Relationship(episode_node, "IS_SUBWORK_OF", parent_work_node))
+
+        for key, episode_node in episode_nodes.items():
+            id_work_parent, season_number, episode_number = key
+
+            if None in (id_work_parent, season_number, episode_number):
+                continue
+            
+            next_episode_node = episode_nodes.get((id_work_parent, season_number, episode_number + 1))
+            if next_episode_node:
+                graph.create(Relationship(episode_node, "NEXT_EPISODE", next_episode_node))
+
+        print("Inserted episodes into Neo4j")
+
+def migrate_genre(pg_connect, graph) -> None:
+    with pg_connect.cursor(name='genre_cursor') as cur:
+        cur.execute(
             """
-# for this one, you will create a nodeType Episode (it seems that every episode have their own id_work)
-# then create the relations 
-#   (:Episode)-[:IsWork]->(:Work)
-#   (:Episode)-[:IsSubWorkOf]->(:Work)
-#   (:Episode)-[:NextEpisode or :LastEpisode]->(:Episode)
-#       # pick the easiest between Next and Last
-#       # you need to find the episode with number +- 1 of episode in the same seaosn with same id_work_parent
-
-
-
-
-def migrate_genre(pg_connect, graph) -> None: pass
-"""
             SELECT 
             id_work, genre
             FROM work_genres
-            """
-# create (:Work)-[:HasGenre]->(:Genre)
-# do not create twice a :Genre, re-use one if you already have created it
+            """)
 
-def migrate_work_type(pg_connect, graph) -> None: pass
-"""
+        while True:
+            genres = cur.fetchmany(1000)
+            if not genres:
+                break
+
+            for genre_data in genres:
+                genre = Genre(*genre_data)
+                genre_node = graph.nodes.match("Genre", name=genre.genre).first()
+                if not genre_node:
+                    genre_node = Node("Genre", name=genre.genre)
+                    graph.create(genre_node)
+                work_node = graph.nodes.match("Work", id=genre.id_work).first()
+                if work_node:
+                    graph.create(Relationship(work_node, "HAS_GENRE", genre_node))
+        print("Inserted genres into Neo4j")
+
+def migrate_work_type(pg_connect, graph) -> None:
+    with pg_connect.cursor(name='work_type_cursor') as cur:
+        cur.execute(
+            """
             SELECT 
             id_work, ordering, type
-            FROM work_genres
-            """
-# create (:Work)-[:HasWorkType]->(:WorkType)
-# do not create twice a :WorkType, re-use one if you already have created it
-    # but the :HasWorkType should have the attribute "ordering"
+            FROM work_types
+            """)
+
+        while True:
+            work_types = cur.fetchmany(1000)
+            if not work_types:
+                break
+
+            for work_type_data in work_types:
+                work_type = WorkType(*work_type_data)
+                work_type_node = graph.nodes.match("WorkType", type=work_type.type).first()
+                if not work_type_node:
+                    work_type_node = Node("WorkType", type=work_type.type)
+                    graph.create(work_type_node)
+                work_node = graph.nodes.match("Work", id=work_type.id_work).first()
+                if work_node:
+                    relationship = Relationship(work_node, "HAS_WORK_TYPE", work_type_node)
+                    relationship["ordering"] = work_type.ordering
+                    graph.create(relationship)
+        print("Inserted work types into Neo4j")
 
 #########################################
 
@@ -144,7 +253,7 @@ def main() -> None:
 
     migrate_work(pg_connect, graph)
     migrate_akas(pg_connect, graph)
-    migrate_genre(pg_connect, graph)
+    migrate_episode(pg_connect, graph)
     migrate_genre(pg_connect, graph)
     migrate_work_type(pg_connect, graph)
     migrate_person(pg_connect, graph)
